0149718baceb672db2432721aced72cabd2ccdc6
rahulrajput83 <rahulrajput21112@gmail.com>
2022-08-14 14:19:23 +0530

    new feature

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './redux/reducer';
-import "bootstrap/dist/js/bootstrap.bundle.min";
 
 const store = createStore(reducer);
 const root = ReactDOM.createRoot(document.getElementById('root'));
