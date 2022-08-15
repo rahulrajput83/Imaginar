781a8a0246c485b2ab610be2ec75a7d1595f3fd0
rahulrajput83 <rahulrajput21112@gmail.com>
2022-08-15 21:05:26 +0530

    New Feature

--- Backend/Router/ProfileImg.js
+++ Backend/Router/ProfileImg.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const SignupUserSchema = require('../Schema/Signup')
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+    cloud_name: process.env.cloud_name,
+    api_key: process.env.api_key,
+    api_secret: process.env.api_secret
+});
+
 
 router.post('/profileimg', (req, res) => {
-    SignupUserSchema.updateOne({ _id: req.body.id }, { $set: { 'profileImg': req.body.img } })
-    .then(res.json({ message: 'Uploaded...' }))
-    .catch(res.json({ message: 'Error, Please try again...' }));
+    cloudinary.uploader.upload(req.body.img)
+        .then(result => {
+            SignupUserSchema.updateOne({ _id: req.body.id }, { $set: { 'profileImg': result.secure_url } })
+                .then(res.json({ message: 'Uploaded...' }))
+                .catch(res.json({ message: 'Error, Please try again...' }));
+        })
+        .catch(err => {
+            res.json({ message: 'Failed' });
+        });
 })
 
 module.exports = router;
